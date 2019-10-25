0fc65ce9ed2359e6daebda57aa5ad6d0a5b74c08
ebbmne <ebbmne@foxmail.com>
2019-10-25 23:11:28 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    test
+  </div>
+</template>
+
+<script>
+
+document.addEventListener("DOMContentLoaded", function() {
+  let fontSize = document.documentElement.clientWidth / 10;
+  fontSize = fontSize > 50 ? 50 : fontSize;
+  document.querySelector("html").style.fontSize = fontSize + "px";
+});
+
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
